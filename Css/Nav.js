91efac338199bca757f57af473b70dfe9a991b32
Zaid <coolzaid26@gmail.com>
2021-05-05 14:19:57 +0530

    photo section improve

--- Css/Nav.js
+++ Css/Nav.js
@@ -5,7 +5,7 @@ $("#search-icon").click(function () {
 });
 
 $('.menu-toggle').click(function () {
-    alert("zaid");
+    // alert("zaid");
     $(".nav").toggleClass("mobile-nav");
     $(".page-wrapper").toggleClass("stick");
     // $(".logo").toggleClass("logoMargin");
